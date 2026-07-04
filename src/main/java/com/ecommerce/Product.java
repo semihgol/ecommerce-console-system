dbfc49680f1083530de852d323bcee1dae8112ed
Semih Göl <smhgl.2001@gmail.com>
2026-07-04 11:28:43 +0300

    Initial commit: Add project structure and Product class

---
+++ src/main/java/com/ecommerce/Product.java
@@ -0,0 +1,40 @@
+package com.ecommerce;
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private int stockQuantity;
+
+    public Product(int id, String name, double price, int stockQuantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        if (stockQuantity >= 0) {
+            this.stockQuantity = stockQuantity;
+        } else {
+            System.out.println("Error: Stock quantity cannot be negative");
+        }
+    }
+}
